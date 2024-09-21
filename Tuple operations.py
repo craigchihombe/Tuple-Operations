1b13e7b66215616542f8a02472fa74310f6887ae
craigchihombe <achihombe@gmail.com>
2024-09-21 10:28:25 +0200

    Add files via upload

---
+++ Tuple operations.py
@@ -0,0 +1,19 @@
+#create a tuple with different data types
+tuplex =('Welcome', False ,2.3 , 5)
+print(tuplex)
+
+tuplex=(1,2,3,4,5)
+print(tuplex)
+
+tuplex = tuplex + (6,)
+print(tuplex)
+
+tuple1 = (50,51,52,50,60,70,50)
+print(tuple1.count(50))
+
+tuple2 = (11,22,33,44,55,66,77,88,99)
+_slice = tuple2[2:5]
+print(_slice)
+
+_slice = tuple2[:6]
+print(_slice)
